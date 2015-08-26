db3352c17a924738dfbf1ca6d6ef5e3d4496f8dc
BrooksPatton <brooks-patton@sbcglobal.net>
2015-08-26 10:38:54 -0600

    user model created

---
+++ models/user.js
@@ -0,0 +1,48 @@
+// static variable declaration
+
+
+// npm requires
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// local requires
+
+
+// variable declarations
+var UserSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
+
+// callback function declarations
+var preSave = function(callback){
+	var user = this;
+	
+	if(!user.isModified('password')) return callback();
+	
+	bcrypt.genSalt(5, function(err, salt){
+		if(err) return callback(err);
+		
+		bcrypt.hash(user.password, salt, null, function(err, hash){
+			if(err) return callback(err);
+			
+			user.password = hash;
+			callback();
+		});
+	});
+};
+
+// function declarations
+
+
+// main
+UserSchema.pre('save', preSave);
+
+module.exports = mongoose.model('User', UserSchema);
